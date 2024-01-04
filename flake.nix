cd3ba28b880de3b241ffb20c51d019c717a2a8ba
Kazuki Okamoto <kazuki.okamoto@kakkun61.com>
2024-01-05 01:45:22 +0900

    freertos

--- flake.nix
+++ flake.nix
@@ -7,33 +7,42 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
+        mkEspDerivation = f: pkgs.stdenv.mkDerivation (finalAttrs:
+          let set = f finalAttrs;
+          in {
+            inherit (set) pname version installPhase;
+            src = pkgs.fetchurl {
+              url = "https://github.com/espressif/esp-idf/archive/refs/tags/v${finalAttrs.version}.tar.gz";
+              hash = "sha256-1xfZ1CulDdDo08x+w2oYPpbHzOQdsWrTFdWtu4XKlns=";
+            };
+          });
       in
       {
         name = "esp";
         packages = {
           default = self.packages.${system}.esp_common;
-          esp_common = pkgs.stdenv.mkDerivation (finalAttrs: {
+          esp_common = mkEspDerivation (finalAttrs: {
             pname = "esp_common";
             version = "4.3.7";
-            src = pkgs.fetchurl {
-              url = "https://github.com/espressif/esp-idf/archive/refs/tags/v${finalAttrs.version}.tar.gz";
-              hash = "sha256-1xfZ1CulDdDo08x+w2oYPpbHzOQdsWrTFdWtu4XKlns=";
-            };
             installPhase = ''
               mkdir -p $out
               cp -r components/esp_common/* $out
             '';
           });
-          xtensa = pkgs.stdenv.mkDerivation (finalAttrs: {
-            pname = "esp_common";
+          xtensa = mkEspDerivation (finalAttrs: {
+            pname = "xtensa";
             version = "4.3.7";
-            src = pkgs.fetchurl {
-              url = "https://github.com/espressif/esp-idf/archive/refs/tags/v${finalAttrs.version}.tar.gz";
-              hash = "sha256-1xfZ1CulDdDo08x+w2oYPpbHzOQdsWrTFdWtu4XKlns=";
-            };
             installPhase = ''
               mkdir -p $out
               cp -r components/xtensa/* $out
+            '';
+          });
+          freertos = mkEspDerivation (finalAttrs: {
+            pname = "freertos";
+            version = "4.3.7";
+            installPhase = ''
+              mkdir -p $out
+              cp -r components/freertos/* $out
             '';
           });
         };
