302efd6b7c77bc373259b67fb3725d0338d6f4eb
Kazuki Okamoto <kazuki.okamoto@kakkun61.com>
2024-01-05 01:13:12 +0900

    xtensa

--- flake.nix
+++ flake.nix
@@ -24,6 +24,18 @@
               cp -r components/esp_common/* $out
             '';
           });
+          xtensa = pkgs.stdenv.mkDerivation (finalAttrs: {
+            pname = "esp_common";
+            version = "4.3.7";
+            src = pkgs.fetchurl {
+              url = "https://github.com/espressif/esp-idf/archive/refs/tags/v${finalAttrs.version}.tar.gz";
+              hash = "sha256-1xfZ1CulDdDo08x+w2oYPpbHzOQdsWrTFdWtu4XKlns=";
+            };
+            installPhase = ''
+              mkdir -p $out
+              cp -r components/xtensa/* $out
+            '';
+          });
         };
         formatter = pkgs.nixpkgs-fmt;
       }
